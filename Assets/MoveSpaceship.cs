f45e584bc2ad96d0ac90b5ffd3e46670162b6472
Arjun Kapoor <kaparjun199@gmail.com>
2020-06-08 16:25:28 +0530

    assets

---
+++ Assets/MoveSpaceship.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveSpaceship : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine("Move");
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * 3f * Time.deltaTime);
+    }
+
+    private IEnumerator Move()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3.5f);
+            transform.eulerAngles += new Vector3(0, 180f, 0);
+        }
+    }
+}
---
+++ Assets/WebCamScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class WebCamScript : MonoBehaviour
+{
+    public GameObject webCameraPlane;
+
+    private void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            GameObject cameraParent = new GameObject("camParent");
+            cameraParent.transform.position = transform.position;
+            transform.parent = cameraParent.transform;
+            cameraParent.transform.Rotate(Vector3.right, 90);
+        }
+
+        Input.gyro.enabled = true;
+
+        WebCamTexture webCameraTexture = new WebCamTexture();
+        webCameraPlane.GetComponent<MeshRenderer>().material.mainTexture = webCameraTexture;
+        webCameraTexture.Play();
+    }
+
+    private void Update()
+    {
+        Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+        transform.localRotation = cameraRotation;
+    }
+}
